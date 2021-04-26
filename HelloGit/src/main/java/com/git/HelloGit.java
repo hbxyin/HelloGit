c3bb22c537ff906ef07386ab227789e9e11cb799
hbxyin <hbxyin@outlook.com>
2021-04-26 22:00:06 +0800

    clone add

--- HelloGit/src/main/java/com/git/HelloGit.java
+++ HelloGit/src/main/java/com/git/HelloGit.java
@@ -2,7 +2,7 @@ package com.git;
 
 /**
  * @author Arbot
- * @create 2021-04-26 19:36
+ * @since  2021-04-26 19:36
  */
 public class HelloGit {
     public static void main(String[] args) {
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("branche add");
         System.out.println("main add");
         System.out.println("change online");
+        System.out.println("clone add");
     }
 }
